cecf9d3b2841ec9ef3759856768d29006fadf647
JeenMaharjan <jeenmaharjan08@gmail.com>
2022-12-26 23:31:16 +0545

    firstt commit

---
+++ src/redux/userSlice.js
@@ -0,0 +1,42 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from "axios"
+
+export const updateUser2 = createAsyncThunk("users/update", async(user) => {
+    const response = await axios.post(
+        "http://localhost:8000/api/users/1/update",
+        user
+    );
+    return response.data;
+});
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState: {
+        userInfo: {
+            name: "john1",
+            email: "jeenmaharjan08@gmail.com"
+        },
+        pending: null,
+        error: false
+    },
+    reducers: {
+
+    },
+    extraReducers: {
+        [updateUser2.pending]: (state) => {
+            state.pending = true
+            state.error = false
+        },
+        [updateUser2.fulfilled]: (state, action) => {
+            state.pending = false
+            state.userInfo = action.payload
+        },
+        [updateUser2.rejected]: (state) => {
+            state.pending = null
+            state.error = true
+        },
+    }
+})
+
+export const { updateStart, updateSuccess, updateError } = userSlice.actions
+export default userSlice.reducer
